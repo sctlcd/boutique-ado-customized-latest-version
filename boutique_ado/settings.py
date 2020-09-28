45e4e7bac5bba875c9e7fdc94935407bdce78898
8 <lucinda.souchet@outlook.com>
2020-09-28 23:19:01 +0100

    added products app, models and fixtures

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -98,6 +98,18 @@ DATABASES = {
     }
 }
 
+SITE_ID = 1
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
+ACCOUNT_USERNAME_MIN_LENGTH = 4
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
