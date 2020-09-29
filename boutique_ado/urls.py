0de0255bd7c122ae80533ff9aab9fd90cd020f7a
8 <lucinda.souchet@outlook.com>
2020-09-29 01:05:14 +0100

    added products views and templates

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -15,9 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -25,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+    path('products', include('products.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
