a332d6a6c82e3fefe8d114bf9ed8e25e2b4e08f6
8 <lucinda.souchet@outlook.com>
2020-10-15 14:21:35 +0100

    set up environement variable

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Boutique Ado v1
+
+### Code
+- Environment variables - [Igor Basuga](https://github.com/bravoalpha79) Tutor at [Code Institute](http://codeinstitute.net)
--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
+if os.path.exists("env.py"):
+    import env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c5uykv54wh%t*n4d-_5fr&h(_gc=_50b4zr#t(nyj9%&kdt#l*'
+# SECRET_KEY = 'c5uykv54wh%t*n4d-_5fr&h(_gc=_50b4zr#t(nyj9%&kdt#l*'
+SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -168,5 +172,9 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# Stripe
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
