5cf9a605283c93b722f32cc7d788085fd8cbf33e
8 <lucinda.souchet@outlook.com>
2020-10-20 15:15:56 +0100

    add checkout form data caching in payment intent

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '066aa6e8f642.ngrok.io']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '9683c02f5758.ngrok.io']
 
 
 # Application definition
