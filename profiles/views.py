7228ee53cd7512310c5f10bb75096fb4ca6292f5
8 <lucinda.souchet@outlook.com>
2020-10-27 23:05:57 +0000

    Product Admin - Securing the Views

--- profiles/views.py
+++ profiles/views.py
@@ -7,6 +7,7 @@ from .forms import UserProfileForm
 from checkout.models import Order
 
 
+@login_required
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
