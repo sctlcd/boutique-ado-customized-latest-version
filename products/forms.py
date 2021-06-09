516357837e30d0e60f490df13e545621bf73e486
8 <lucinda.souchet@outlook.com>
2021-06-09 16:43:20 +0200

    update products forms.py: add placeholders and classes, set autofocus on first field

--- products/forms.py
+++ products/forms.py
@@ -7,15 +7,32 @@ class ProductForm(forms.ModelForm):
 
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = ('category', 'sku', 'name', 'description', 'has_sizes', 'price', 'image',)
 
     image = forms.ImageField(label="Image", required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
+        """
+            Add placeholders and classes, set autofocus on first field
+        """
         super().__init__(*args, **kwargs)
+        placeholders = {
+            'sku': 'Sku',
+            'name': 'Name',
+            'description': 'Description',
+        }
         categories = Category.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
         self.fields['category'].choices = friendly_names
-        for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'border-black rounded-0'
+
+        self.fields['category'].widget.attrs['autofocus'] = True
+
+        for field in self.fields:
+            if field == 'sku' or field == 'name' or field == 'description':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'border-grey rounded-0'
